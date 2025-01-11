a763d606f55a78643f33f92a19ce91cc6f6b839b
tropical-summer <fuji0620s.1.n@ezweb.ne.jp>
2025-01-11 21:41:22 +0900

    enable to address vectors in commandline args

--- node_options/src/cli_options.cpp
+++ node_options/src/cli_options.cpp
@@ -12,8 +12,7 @@ namespace node_options
 // デフォルト値
 Options::Options()
 {
-    payload_size = 32;
-    period_ms = 10;
+
 }
 
 // コンストラクタ
@@ -26,14 +25,13 @@ Options::Options(int argc, char ** argv)
 // 受け取ったコマンドライン引数をもとに、option変数を更新
 void Options::parse(int argc, char ** argv)
 {
-    // コマンドラインのargsをパース
     cxxopts::Options options(argv[0], "ROS2 performance benchmark");
 
     options.add_options()(
         "node_name", "name for this node", cxxopts::value<std::string>(node_name))(
-        "topic_name", "topic_name for this node", cxxopts::value<std::string>(topic_name))(
-        "s, size", "payload size", cxxopts::value<int>(payload_size)->default_value(std::to_string(payload_size)), "bytes")(
-        "p, period", "publish frequency", cxxopts::value<int>(period_ms)->default_value(std::to_string(period_ms)), "ms_sec"    
+        "topic_names", "topic_name for this node", cxxopts::value<std::vector<std::string>>(topic_names))(
+        "s, size", "payload size", cxxopts::value<std::vector<int>>(payload_size), "bytes")(
+        "p, period", "publish frequency", cxxopts::value<std::vector<int>>(period_ms), "ms_sec"    
         );
 
     try {
@@ -44,7 +42,7 @@ void Options::parse(int argc, char ** argv)
             exit(1);
         }
 
-        if(result.count("topic_name") == 0){
+        if(result.count("topic_names") == 0){
             std::cout << "Please specify the topic_name for this node" << std::endl;
             exit(1);
         }
@@ -57,9 +55,19 @@ void Options::parse(int argc, char ** argv)
 // コマンドラインでの表示を見やすくするためのオーバーロード処理
 std::ostream & operator<<(std::ostream & os, const Options & options)
 {
-    os << "topic_name: " << options.topic_name << std::endl;
-    os << "payload_size: " << options.payload_size << " bytes" << std::endl;
-    os << "period_ms: " << options.period_ms << " miliseconds" <<  std::endl;
+    os << "Node Name: " << options.node_name << std::endl;
+
+    for (size_t i = 0; i < options.topic_names.size(); ++i) {
+        os << "Topic: " << options.topic_names[i] << std::endl;
+
+        if(!options.payload_size.empty()){
+            os << "payload_size: " << options.payload_size[i] << " bytes" << std::endl;
+        }
+
+        if(!options.period_ms.empty()){
+            os << "period_ms: " << options.period_ms[i] << " ms" << std::endl;
+        }
+    }
 
     return os;
 }
