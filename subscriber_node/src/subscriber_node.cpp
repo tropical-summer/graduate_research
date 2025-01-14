9fc7b6a1ca843f50f61e097752beb5ce68602947
tropical-summer <fuji0620s.1.n@ezweb.ne.jp>
2025-01-14 16:13:08 +0900

    Fix start_time

--- subscriber_node/src/subscriber_node.cpp
+++ subscriber_node/src/subscriber_node.cpp
@@ -33,21 +33,26 @@ public:
     // 複数のトピック名を扱う場合
     for (size_t i = 0; i < options.topic_names.size(); ++i) {
       const std::string & topic_name = options.topic_names[i];
+      start_time_[topic_name] = this->get_clock()->now();
 
-      auto callback = [this, topic_name](const publisher_node::msg::IntMessage::SharedPtr message_) -> void{
+      auto callback = [this, topic_name, options](const publisher_node::msg::IntMessage::SharedPtr message_) -> void{
+        // eval_time秒過ぎてたら受け取らず終了
+        auto now = this->get_clock()->now();
+        if((now.seconds() - start_time_[topic_name].seconds()) >= options.eval_time) {
+          RCLCPP_INFO(this->get_logger(), "Topic %s has reached the evaluation time.", topic_name.c_str());
+          return;
+        }
+
         // message_->dataを16進数形式で表示 (0埋めはしない)
         std::ostringstream oss;
         for (const auto& byte : message_->data)
         {
             oss << std::hex << (int)byte << " ";
         }
-
-        const builtin_interfaces::msg::Time time_stamp = message_->header.stamp;
-        oss << std::dec <<"Time: " << time_stamp.sec << "." << time_stamp.nanosec;
-
+        // subした時刻などを表示
+        oss << std::dec <<"Time: " << (now.seconds() - start_time_[topic_name].seconds()) << "." << (now.nanoseconds() - start_time_[topic_name].nanoseconds());
         int current_pub_idx = message_->header.pub_idx;
-
-        RCLCPP_INFO(this->get_logger(), "Topic: %s Data: %s Index: %d", topic_name.c_str(), oss.str().c_str(), current_pub_idx);
+        RCLCPP_INFO(this->get_logger(), "Subscribed/ Topic: %s Data: %s Index: %d", topic_name.c_str(), oss.str().c_str(), current_pub_idx);
       };
         
       rclcpp::QoS qos(rclcpp::KeepLast(10));
@@ -61,7 +66,7 @@ public:
 private:
   // トピックごとのPublisher
   std::unordered_map<std::string, rclcpp::Subscription<publisher_node::msg::IntMessage>::SharedPtr> subscribers_;
-  publisher_node::msg::IntMessage::SharedPtr message_;
+  std::unordered_map<std::string, rclcpp::Time> start_time_;
 };
 
 int main(int argc, char * argv[])
